793405a4719c95961ced8edfbb7f036970bc5198
jajanidzee <jajanidzekote310@gmail.com>
2026-06-01 14:58:49 +0400

    .

---
+++ app/Components/CarrersComponents/Coaching/Coaching.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Coaching = () => {
+  return (
+    <div className="w-[1682px] rounded-[37px] bg-[#252641] pt-[70px] pb-[83px] px-[212px] text-center mt-[197px] ">
+      <h4 className="font-[Poppins] font-semibold text-[36px] text-[#FFFFFF]">
+        Online coaching lessons for remote learning.
+      </h4>
+      <p className="font-[Poppins] font-normal text-[24px] mt-[30px] text-[#FFFFFF]">
+        Interactive online coaching lessons designed for remote learning,
+        helping students build skills, gain confidence, and achieve academic
+        success from anywhere.
+      </p>
+      <button className="w-[236px] h-[63px] bg-[#49BBBD] text-[#fff] rounded-[12px] mt-[73px] font-[Poppins] text-[16px] text-bold">
+        Start learning now
+      </button>
+    </div>
+  );
+};
+
+export default Coaching;
--- app/carrers/page.tsx
+++ app/carrers/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Components/Header/Header";
 import Pricing from "../Components/CarrersComponents/Pricing/Pricing";
+import Coaching from "../Components/CarrersComponents/Coaching/Coaching";
 
 const carrersRouting = () => {
   return (
-    <div className=" w-[1912px] m-auto ">
+    <div className=" w-[1912px] m-auto">
       <Header />
       <Pricing />
+      <Coaching />
     </div>
   );
 };
